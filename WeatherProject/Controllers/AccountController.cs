13cf4a2e0ef8001a122e06ee41b55a88a5bc5997
Logan Sibinski <vi8525gc@go.minnstate.edu>
2022-03-05 07:58:07 -0600

    Add project files.

---
+++ WeatherProject/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherProject.Models;
+using WeatherProject.Models.ViewModels;
+using WeatherProject.Helper;
+
+namespace WeatherProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(ApplicationDBContext db, UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid login attempt");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logoff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ WeatherProject/Helper/Helper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherProject.Helper
+{
+    public static class Helper
+    {
+        public static string Admin = "Admin";
+        public static string Employee = "Employee";
+
+        public static List<SelectListItem> GetRolesForDropDown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value=Helper.Admin,Text=Helper.Admin},
+                new SelectListItem{Value=Helper.Employee,Text=Helper.Employee}
+            };
+        }
+    }
+}
